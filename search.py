10b74e54bb3ed20f87807436485db20f48150e80
dahnny012 <nguy1952@umn.edu>
2015-02-03 13:30:58 -0600

    Prototype code,  logic down

--- search.py
+++ search.py
@@ -1,15 +1,36 @@
 import urllib2
 import urllib
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
-url = 'https://www.mangaupdates.com/search.html'
-values = {'search' : 'ao haru ride'}
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
 
-response = urllib2.urlopen(req)
-the_page = response.read()
+pool = Pool(10)
+p.map(getChapters,[[0,100],[0,200]])
 
-soup=BeautifulSoup(the_page)
+def getChapters(start,stop)
+	# Define a start and stop
+	# till N
+	for chapter in list:
+		results = searchManga(chapter)
+		scanner = parseResults(results)
+	
+		
 
-print(soup.body)
+
+
+def searchManga(manga):
+	url = 'https://www.mangaupdates.com/search.html'
+	values = {'search' : manga}
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
+	the_page = response.read()
+	soup=BeautifulSoup(the_page)
+	return soup
+	
+def parseResults(soup):
+	# Look for results
+	# Record them
+	# do somethign with them
+	return 1
+	
