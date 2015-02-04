b5539fd3fdbf3067db96fda41637e07f4739a04f
dahnny012 <nguy1952@umn.edu>
2015-02-04 12:13:52 -0600

    started on html dom parse

--- search.py
+++ search.py
@@ -1,22 +1,27 @@
 import urllib2
 import urllib
-from bs4 import BeautifulSoup
 from multiprocessing import Pool
+from HTMLParser import HTMLParser
 
+##
+##pool = Pool(10)
+##p.map(getChapters,[[0,100],[0,200]])
 
-pool = Pool(10)
-p.map(getChapters,[[0,100],[0,200]])
-
-def getChapters(start,stop)
+#def getChapters(start,stop)
 	# Define a start and stop
 	# till N
-	for chapter in list:
-		results = searchManga(chapter)
-		scanner = parseResults(results)
-	
-		
+#	for chapter in list:
+#		results = searchManga(chapter)
+#		scanner = parseResults(results)
 
 
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+	print(attrs)
+			
+        
+ 
+parser = MyHTMLParser()
 
 def searchManga(manga):
 	url = 'https://www.mangaupdates.com/search.html'
@@ -25,12 +30,16 @@ def searchManga(manga):
 	req = urllib2.Request(url, data)
 	response = urllib2.urlopen(req)
 	the_page = response.read()
-	soup=BeautifulSoup(the_page)
+	soup = parser.feed(the_page)
 	return soup
 	
 def parseResults(soup):
-	# Look for results
-	# Record them
-	# do somethign with them
+	results = soup.find_all('a');
+	for link in results:
+		if 'key1' in link.keys():
+			print(link['title'])
 	return 1
 	
+		
+results = searchManga("Ao haru ride")
+#parseResults(results)
