dd9123d4abc039a8977e5ead75590e2e8111a8b7
Danh Nguyen <filler012@gmail.com>
2015-02-13 12:26:12 +0000

    Made into a module

--- search.py
+++ search.py
@@ -18,80 +18,75 @@ class TagStripper(HTMLParser):
 		return " ".join(self.fed)
 	def flush_buffer(self):
 		self.fed = []
+
+class search():
+	def __init__(self,idList):
+		self.idList = idList
+	def run(self):
+		threads = []
+		t0 = time.time()
+		for id in self.idList:
+			t = Thread(target=self.searchReleases, args=(id,))
+			threads.append(t)
+			t.start()
+		for i in range(0,len(self.idList)):
+			threads[i].join()
+		print("Finished in: ")
+		print time.time() - t0
+	def searchReleases(self,id):
+		releases = True
+		page = 1
+		parser = TagStripper()
+		while releases:
+			releasesLink = self.seriesIdToReleases(id,page)
+			releasesPage = self.getPage(releasesLink)
+			releases = self.buildReleasesTable(releasesPage,parser);
+			page = page + 1
 		
-def searchReleases(id):
-	releases = True
-	page = 1
-	parser = TagStripper()
-	while releases:
-		releasesLink = seriesIdToReleases(id,page)
-		releasesPage = getPage(releasesLink)
-		releases = buildReleasesTable(releasesPage,parser);
-		print(releases);
-		page = page + 1
-	
+			
+	def seriesIdToReleases(self,seriesId,page=1):
+		page = str(page)
+		base = "https://www.mangaupdates.com/releases.html?page="+page
+		base = base +"&search="
+		end = "&stype=series"
+		return base + seriesId + end
 		
-def seriesIdToReleases(seriesId,page=1):
-	page = str(page)
-	base = "https://www.mangaupdates.com/releases.html?page="+page
-	base = base +"&search="
-	end = "&stype=series"
-	return base + seriesId + end
+		
+	def buildReleasesTable(self,page,parser):
+		#Find table of releases
+		iterr = re.finditer("<td class='text pad'(.)*</td>",page)
+		header = 0
+		row = []
+		try:
+			#If there is a release
+			iterr.next().start(0)
+		except Exception, e:
+			return False
+		for m in iterr:
+			#For each table element strip it and add it to the list
+			releaseNode = page[m.start(0):m.end(0)]
+			release = self.stripTags(releaseNode,parser)
+			row.append(release)
+			header = header + 1
+			#Dump the list to csv after we get the release info
+			if(header == 5):
+				with open('test.csv','a') as csvfile:
+					writer = csv.writer(csvfile)
+					writer.writerow(row)
+				row = []
+			header = header % 5
+		return True
+		
+	def stripTags(self,node,parser):
+		parser.feed(node)
+		data = parser.get_data()
+		parser.flush_buffer()
+		return data
 	
+	def findSeriesID(self,a):
+		return a.split("id=")[1]
 	
-def buildReleasesTable(page,parser):
-	#Find table of releases
-	iterr = re.finditer("<td class='text pad'(.)*</td>",page)
-	header = 0
-	row = []
-	try:
-		#If there is a release
-		iterr.next().start(0)
-	except Exception, e:
-		return False
-	for m in iterr:
-		#For each table element strip it and add it to the list
-		releaseNode = page[m.start(0):m.end(0)]
-		release = stripTags(releaseNode,parser)
-		row.append(release)
-		header = header + 1
-		#Dump the list to csv after we get the release info
-		if(header == 5):
-			with open('test.csv','a') as csvfile:
-				writer = csv.writer(csvfile)
-				writer.writerow(row)
-			row = []
-		header = header % 5
-	return True
-	
-def stripTags(node,parser):
-	parser.feed(node)
-	data = parser.get_data()
-	parser.flush_buffer()
-	return data
-
-def findSeriesID(a):
-	return a.split("id=")[1]
-
-def getPage(url):
-	req = urllib2.Request(url)
-	response = urllib2.urlopen(req)
-	return response.read()
-	
-
-
-threads = []
-releases = ["1","2","3","4","5"]
-t0 = time.time()
-for i in range(5):
-	t = Thread(target=searchReleases, args=(releases[i],))
-	threads.append(t)
-	t.start()
-	
-threads[0].join()
-threads[1].join()
-threads[2].join()
-threads[3].join()
-threads[4].join()
-print("Finished in: ")
-print time.time() - t0
+	def getPage(self,url):
+		req = urllib2.Request(url)
+		response = urllib2.urlopen(req)
+		return response.read()
