60d1fe707fd4a64ec4535f54155dc4a29ea9cb25
Danh Nguyen <filler012@gmail.com>
2015-02-13 14:26:56 +0000

    thread pool done i think

--- search.py
+++ search.py
@@ -7,7 +7,8 @@ import csv
 import time
 
 
-lock = threading.Lock()
+fileLock = threading.Lock()
+queueLock = threading.Lock()
 class TagStripper(HTMLParser):
 	def __init__(self):
 		self.reset()
@@ -25,15 +26,24 @@ class search():
 	def run(self):
 		threads = []
 		t0 = time.time()
-		for id in self.idList:
-			t = threading.Thread(target=self.searchReleases, args=(id,))
+		numThreads = 3
+		for id in range(0,numThreads):
+			t = threading.Thread(target=self.searchReleases, args=(self.idList,))
 			threads.append(t)
 			t.start()
-		for i in range(0,len(self.idList)):
+		for i in range(0,numThreads):
 			threads[i].join()
 		print("Finished in: ")
 		print time.time() - t0
-	def searchReleases(self,id):
+	def searchReleases(self,idList):
+		exit = False
+		with queueLock:
+			try:
+				id = idList.pop()
+			except:
+				exit = True
+		if exit:
+			return
 		releases = True
 		page = 1
 		parser = TagStripper()
@@ -42,6 +52,7 @@ class search():
 			releasesPage = self.getPage(releasesLink)
 			releases = self.buildReleasesTable(releasesPage,parser);
 			page = page + 1
+		self.searchReleases(idList)
 		
 			
 	def seriesIdToReleases(self,seriesId,page=1):
@@ -70,7 +81,7 @@ class search():
 			header = header + 1
 			#Dump the list to csv after we get the release info
 			if(header == 5):
-				with lock:
+				with fileLock:
 					with open('test.csv','a') as csvfile:
 						writer = csv.writer(csvfile)
 						writer.writerow(row)
@@ -95,7 +106,7 @@ class search():
 		
 		
 		
-scraper = search(["1","2","3","4","5"])
+scraper = search(["1","2","3","4"])
 scraper.run()
 		
 		
