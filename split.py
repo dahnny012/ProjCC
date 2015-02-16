2d39222051238116545d124394cb323b18d3bf68
sudowoodo <giraffapotamus@gmail.com>
2015-02-16 12:19:56 -0600

    split.py pads num, num before ext, and cmd args
    call 'python split.py seriesIDs.txt 1000'

--- split.py
+++ split.py
@@ -1,16 +1,25 @@
-import io,os,sys
+import io,os,sys,re
 
+def padNum(number, characters):
+	return "0"*(characters - len(str(number))) + str(number)
 
 def split(filename,numLines):
     list = []
+    numLines = int(numLines)
     with open(filename,"r") as file:
         list = file.readlines()
+        
+    extRegex = re.compile('\.[A-Za-z]+$', re.IGNORECASE)
+    fileExt = extRegex.search(filename)
+    fileExt = fileExt.group(0)
+    filename = extRegex.sub('', filename)
+    
     start = 0
     stop = 0
     numFiles = 1
     for id in list:
-        if stop == numLines:
-            with open(filename + str(numFiles),"w") as file:
+        if stop%numLines == numLines-1:
+            with open(filename + padNum(numFiles, 3) + fileExt,"w") as file:
                 file.write("".join(list[start:stop]))
             start = stop
             numFiles = numFiles + 1
@@ -18,7 +27,10 @@ def split(filename,numLines):
         
     #Last Remaining
     if start != stop:
-        with open(filename + str(numFiles),"w") as file:
+        with open(filename + padNum(numFiles, 3) + fileExt,"w") as file:
                 file.write("".join(list[start:stop]))
-split("seriesIDs.txt",10000)
-                
+
+filename = sys.argv[1]
+numLines = sys.argv[2]
+
+split(filename, numLines)
