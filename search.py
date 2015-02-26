75f19d8c1af2db39dbf0175b7143705687b988a6
sudowoodo <giraffapotamus@gmail.com>
2015-02-25 21:14:11 -0600

    More minor changes to search.py
    Changed how default files are handled when no additional arguments are given.
    Combined error/log writing into one function, lockWrite.
    Changed quote-wrapping, so that it only wraps strings that need it, in order to decrease filesize and the amount of writing.
    Ignoring dates, to decrease filesize and the amount of writing.
    Checks for whether there is an existing output csv, and appends a number to the end if there is.

--- search.py
+++ search.py
@@ -1,10 +1,25 @@
+# Multithreaded MangaUpdates scraper
+# Checks whether each series id is valid
+# Takes the series name from the series page
+# Pulls release information from the release pages
+
+# Encodes data in UTF-8, wraps it in double-quotes, delimits it with commas
+# Writes to a csv with the same name as the input file
+
+# Accepts any number of input files as command line arguments
+# If no arguments are given, it defaults to "uniqueSeries.txt"
+# It expects input files to be plain-text with one series ID per line
+
 import urllib
 import threading
 from HTMLParser import HTMLParser
 import re
 import time
-import io,os,sys
+import sys, os
+# codecs allows us to write to files in UTF-8
+# It is unfortunately incompatible with the csv library
 import codecs
+# repairUnicode is an outside library (recipe?) I ripped
 import repairUnicode
 
 fileLock = threading.Lock()
@@ -12,30 +27,35 @@ queueLock = threading.Lock()
 logLock = threading.Lock()
 errorLock = threading.Lock()
 
-nameReg    = re.compile("(?<=<title>Baka\-Updates Manga \- ).+(?=</title>)")
-releaseReg = re.compile("<td class='text pad'.*?</td>")
-nodeReg    = re.compile("(?<=>).*?(?=<)")
+extReg     = re.compile('\.[A-Za-z]+$')
+nameReg    = re.compile('(?<=<title>Baka\-Updates Manga \- ).+(?=</title>)')
+releaseReg = re.compile('<td class=\'text pad\'.*?</td>')
+nodeReg    = re.compile('(?<=>).*?(?=<)')
+
+# NOTE: groupReg contains two parenthesized groups
+# so we can call group(0) for the entire match,
+# or group(1) for the gID and group(2) for the gName
 groupReg   = re.compile("((?<=id=)[0-9]+)\' title=\'Group Info\'>(.*?(?=</a>))")
-# Note that groupReg contains two parenthesized groups,
-# so we can call group(0) for the entire match, or group(1) for the gID and group(2) for the gName
+
 
 class search():
 	def __init__(self, idList=None):
-		self.idList = idList
-		self.filename = "default"
+		self.idList     = idList
 		self.htmlParser = HTMLParser()
+		self.filename   = "uniqueSeries.txt"
 
-	def readFile(self, filename):
+	def readFile(self, filename=None):
+		if filename == None:
+			filename = self.filename
 		with open(filename, 'r') as file:
 			self.idList = [line.rstrip('\n') for line in file]
 		print("File: %s" % filename)
-		self.filename = re.sub('\.[A-Za-z]+$', '', filename)
+		self.filename = extReg.sub('', filename)
 
-		# Clear the log
-		# Clear the csv and write the header row
-		self.clearFile(self.filename + ".log")
-		self.clearFile(self.filename + ".csv")
-		self.writeRow(self.filename + ".csv", ["SERIES_ID", "DATE", "SERIES_NAME", "VOLUME", "CHAPTER", "GROUP_NAME", "GROUP_ID"])
+		# Check for existing output and append a number to the filename if necessary
+		# Write the header row to the csv
+		self.filename = self.checkFilename(self.filename, ".csv")
+		self.writeRow(self.filename + ".csv", ["SERIES_ID", "SERIES_NAME", "VOLUME", "CHAPTER", "GROUP_NAME", "GROUP_ID"])
 
 	# Multithread and wait for them to finish executing
 	def run(self):
@@ -75,18 +95,31 @@ class search():
 				releasesPage = self.getPage(releasesLink)
 				releases = self.buildReleasesTable(releasesPage, sId, sName);
 				page += 1
-			self.log(sId)
+			# self.log(sId)
+			self.lockWrite(sId, logLock, "_log.txt")
 			
 	# "An abstraction to hide stuff"
 	def seriesIdToReleases(self, sId, page=1):
 		page = str(page)
 		URL = "https://www.mangaupdates.com/releases.html?page=" + page +"&search=" + sId + "&stype=series"
 		return URL
-		
+
 	# Open and close
 	def clearFile(self, filename):
 		file1 = open(filename, 'w')
 		file1.close()
+
+	# If the filename already exists, append a number
+	def checkFilename(self, filename, fileExt):
+		if os.path.isfile(filename + fileExt):
+			count = 0
+			while True:
+				count += 1
+				countStr = str(count)
+				if not os.path.isfile(filename + countStr + fileExt):
+					return filename + countStr
+		else:
+			return filename
 
 	# Return the series name from the sId
 	# If the page doesn't exist, log it and return False
@@ -99,13 +132,12 @@ class search():
 			return sName
 		else:
 			print("Bad series ID: " + sId)
-			with errorLock:
-				with open('badSeriesIDs.txt', 'a') as badIDFile:
-					badIDFile.write(sId + '\n')
+			self.lockWrite(sId, errorLock, '_bad.txt')
 			return False
 
 		return False
 		
+	# Scrape one page of releases
 	def buildReleasesTable(self, page, sId, sName = None):
 		# Converting to a list is an easy way to check len()
 		nodes = list(re.finditer("<td class='text pad'(.)*</td>",page))
@@ -118,7 +150,11 @@ class search():
 			for node in nodes:
 				header += 1
 
-				if header == 2 and sName != None:
+				# Skip date
+				if header == 1:
+					continue
+				# Use the name from the series page, not the releases page
+				elif header == 2:
 					row.append(sName)
 				elif header < 5:
 					releaseNode = node.group(0)
@@ -186,25 +222,26 @@ class search():
 	def encodeStr(self, inStr):
 		try:
 			outStr = inStr.encode('utf-8')
-			outStr = '"' + outStr + '"'
+			if outStr.find('"') != -1 or outStr.find(',') != -1:
+				outStr = outStr.replace('"', '""')
+				outStr = '"' + outStr + '"'
+			
 			outStr = outStr.decode('utf-8')
 			outStr = repairUnicode.fix_bad_unicode(outStr)
 		except:
 			print("Failure to encode: %s" % inStr)
-			return False
+			return ""
 		return outStr
-
-	def findSeriesID(self, a):
-		return a.split("id=")[1]
 	
-	def getPage(self, url):
+	def getPage(self, url, recur=0):
 		try:
 			response = urllib.urlopen(url)
 			page = response.read()
 		except:
 			# It's all good, just keep trying.
-			print("Retrying %s" % url)
-			return self.getPage(url)
+			recur += 1
+			print("No response. Retry #%d on\n %s" % (recur, url))
+			return self.getPage(url, recur)
 
 		try:
 			# Decode latin-1 and unescape HTML entities like &lt;
@@ -215,11 +252,10 @@ class search():
 
 		return page
 
-	def log(self, sId):
-		with logLock:
-			with open(self.filename + ".log",'a') as outFile:
+	def lockWrite(self, sId, lock, fileExt):
+		with lock:
+			with open(self.filename + fileExt, 'a') as outFile:
 				outFile.write(sId + "\n")
-
 
 # Script execution
 # Check for commandline arguments
@@ -232,5 +268,5 @@ if argLen > 1:
 else:
 	print ("Usage: 'python search.py <filename> [<filename 2> ... <filename n>]'")
 	print "Default",
-	scraper.readFile("uniqueSeries.txt")
+	scraper.readFile()
 	scraper.run()
